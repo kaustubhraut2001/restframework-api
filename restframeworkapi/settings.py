82be3b03d1670a3b25f1e07798a2a4a45eec4a9e
Kaustubh <kaustubhr2001@gmail.com>
2024-03-28 17:00:54 +0530

    renderdb

--- restframeworkapi/settings.py
+++ restframeworkapi/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+print(env("DATABASE_URL"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +84,11 @@ WSGI_APPLICATION = 'restframeworkapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+	"default": dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
